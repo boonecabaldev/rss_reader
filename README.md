949b2a7d87d7b5bfc9f409ebedb26aad9b524be4
boonecabaldev <164784238+boonecabaldev@users.noreply.github.com>
2024-06-10 18:06:52 +0000

    working on db

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rss_reader
+
+```sh
+git clone https://github.com/boonecabaldev/dotfiles.git
+
+cd dotfiles/
+chmod +x install.sh
+./install.sh
+. .bashrc
+cd ..
+```
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import feedparser
 import os
+import csv
 from xml.etree import ElementTree as ET
+import collections
+import pandas as pd
+import numpy as np
 
 
 def download_opml(opml_path):
@@ -17,7 +21,7 @@ def download_opml(opml_path):
         return None  # Return None to indicate failure
 
 
-def extract_rss_feeds(opml_content):
+def opml_to_xml(opml_content):
     """Extracts RSS feed URLs from the OPML content."""
     if not opml_content:  # Handle None value from download_opml
         return []
@@ -25,33 +29,43 @@ def extract_rss_feeds(opml_content):
     soup = BeautifulSoup(opml_content, 'xml')
     feeds = []
     for outline in soup.find_all('outline', type='rss'):
-        feeds.append(outline['xmlUrl'])
+        yield outline['xmlUrl']
     return feeds
 
 
+def xml_to_entity(feed_url):
+    parsed_feed = feedparser.parse(feed_url)
+
+    Entity = collections.namedtuple('Entity', ['title', 'link', 'summary'])
+    for xml in parsed_feed.entries:
+        yield Entity(xml.title, xml.link, xml.summary)
+
+
+def entity_to_dataframe(df):
+    pass
+
+
 def save_feed_content(feed_url, output_dir):
-    """Fetches and saves the feed content as a formatted XML file."""
     parsed_feed = feedparser.parse(feed_url)
     feed_title = parsed_feed.feed.title
 
+    # Format the XML
     root = ET.Element("feed")
-    ET.SubElement(root, "title").text = feed_title
-
     for entry in parsed_feed.entries:
         item = ET.SubElement(root, "item")
         ET.SubElement(item, "title").text = entry.title
         ET.SubElement(item, "link").text = entry.link
         ET.SubElement(item, "description").text = entry.summary
 
-    # Format the XML
     xml_string = ET.tostring(root, encoding='utf-8')
     xml = bytes_to_string_replace_and_back(xml_string)
     pretty_xml = BeautifulSoup(xml, 'xml').prettify()
-
     # Save to file
     with open(f"{output_dir}/{feed_title}.xml", "w", encoding='utf-8') as file:
         file.write(pretty_xml)
         print(f"Saved RSS file: {output_dir}/{feed_title}.xml")
+
+    return pretty_xml
 
 
 def replace_gt_lt(text):
@@ -70,18 +84,52 @@ def bytes_to_string_replace_and_back(byte_data):
     return string_data
 
 
+def write_rss_to_csv(feed_url):
+    with open('my_data.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        parsed_feed = feedparser.parse(feed_url)
+        feed_title = parsed_feed.feed.title
+
+        #root = ET.Element("feed")
+
+        # Write the header row
+        writer.writerow(['FeedTitle', 'Title', 'Link', 'Summary'])
+        writer.writerow([feed_title])
+        for entry in parsed_feed.entries:
+            #item = ET.SubElement(root, "item")
+            writer.writerow([entry.title])
+            writer.writerow([entry.link])
+            writer.writerow([entry.summary])
+
+
+def clean_entity(entity):
+    pass
+
+
 if __name__ == "__main__":
     opml_path = "rss/feedly_rss.opml"
     output_dir = "feed_outputs"
-
     os.makedirs(output_dir, exist_ok=True)
 
     opml_content = download_opml(opml_path)
-    if opml_content:
-        rss_feeds = extract_rss_feeds(opml_content)
+    for rss_xml in opml_to_xml(opml_content):
+        try:
+            save_feed_content(rss_xml, output_dir)
 
-        for feed_url in rss_feeds:
-            try:
-                save_feed_content(feed_url, output_dir)
-            except Exception as e:
-                print(f"\nUnable to process RSS file: {feed_url}: {e}\n")
+            # df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+            #...                    columns=['a', 'b', 'c'])
+            titles = []
+            links = []
+            summaries = []
+            for entity in xml_to_entity(rss_xml):
+                titles.append(entity.title)
+                links.append(entity.link)
+                summaries.append(entity.summary)
+
+            rss_data = {'Title': titles, 'Link': links, 'Summary': summaries}
+            df = pd.DataFrame(data=rss_data)
+            print(f"\ndf: {df}")
+
+        except Exception as e:
+            print(f"\nUnable to process feed_url: {e}\n")
